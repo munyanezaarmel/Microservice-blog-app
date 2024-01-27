bb9128e81d41dfec61c0bb6cec538cbfdbe0708e
Munyaneza Armel <73389979+munyanezaarmel@users.noreply.github.com>
2024-01-27 10:29:28 +0200

    setting mini microservice blog app

---
+++ comments/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+const commentsById = {};
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsById[req.params.id] || []);
+});
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+  const comments = commentsById[req.params.id] || [];
+  comments.push({ id: commentId, content });
+  commentsById[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(3001, () => {
+  console.log("app is listening on 3001");
+});
